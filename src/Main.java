334d2250dab5fc9a8911cfc7d8ca5bbffbc81819
Арсений <Penkovskiy2024@mail.ru>
2026-01-28 23:49:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//import java.io. File;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//public void Main {
+//    public static void main(String[]){
+//        try {
+//            test();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//  static void test() throws IOException {
+//    PrintWriter writer = new PrintWriter(new PrintWriter("Hi.txt"));
+//    writer.print("Hi");
+//    writer.flush();
+//    writer.close();
+//}
+//}
+
